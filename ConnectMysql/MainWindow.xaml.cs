9906f28e902e14ea9d75321107afc788d4917ec4
981746 <981746@gmail.com>
2015-11-19 16:33:41 +0800

    Initialize

---
+++ ConnectMysql/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+namespace ConnectMysql
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string dbHost = "127.0.0.1";//資料庫位址
+            string dbUser = "root";//資料庫使用者帳號
+            string dbPass = "";//資料庫使用者密碼
+            string dbName = "api";//資料庫名稱
+
+            string connStr = "server=" + dbHost + ";uid=" + dbUser + ";pwd=" + dbPass + ";database=" + dbName;
+            Console.WriteLine("連接字串："+ connStr);
+            MySqlConnection conn = new MySqlConnection(connStr);
+            MySqlCommand command = conn.CreateCommand();
+            conn.Open();
+
+            String cmdText = "select * from developer";
+            MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+            MySqlDataReader reader = cmd.ExecuteReader(); //execure the reader
+            while (reader.Read())
+            {
+                for (int i = 0; i < 3; i++)//i代表column數量
+                {
+                    String s = reader.GetString(i);
+                    Console.Write(s + "\t");
+                }
+                Console.Write("\n");
+            }
+
+
+            Console.ReadLine();
+            conn.Close();
+
+        }
+    }
+}
